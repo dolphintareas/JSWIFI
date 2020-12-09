fe7c38af9bed4033d565b0a31887f1e55236edfd
dolphintareas <dolphintareas@gmail.com>
2020-12-08 23:33:56 -0500

    8dic---

--- src/server.js
+++ src/server.js
@@ -55,10 +55,12 @@ app.use((req, res, next) =>{
 
 //Routes
 app.use(require('./routes/index.routes'));
+app.use(require('./routes/auth.routes'));
 app.use(require('./routes/notes.routes'));
 app.use(require('./routes/users.routes'));
 app.use(require('./routes/cursos.routes'));
-app.use(require('./routes/auth.routes'));
+app.use(require('./routes/inscripciones.routes'));
+
 
 
 //Static files
