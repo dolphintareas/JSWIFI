d19990891b71e13a90ff7eab988339b4d909da6b
lgonx <dolphintareas@gmail.com>
2022-12-13 20:53:30 -0500

    13dic

--- src/server.js
+++ src/server.js
@@ -69,10 +69,10 @@ app.use((req, res, next) =>{
 //Routes
 app.use(require('./routes/inscripciones.routes'));
 app.use(require('./routes/index.routes'));
-app.use(require('./routes/auth.routes'));
-app.use(require('./routes/notes.routes'));
-app.use(require('./routes/users.routes'));
-app.use(require('./routes/cursos.routes'));
+//app.use(require('./routes/auth.routes'));
+//app.use(require('./routes/notes.routes'));
+//app.use(require('./routes/users.routes'));
+//app.use(require('./routes/cursos.routes'));
 
 
 
